e3d7107a049b2ac7bee80a922997b2cf6faa6f0c
Colin Hayes <colin.hayes985@gmail.com>
2018-06-26 14:48:48 -0400

    Bonus Credit-Add second heading and button to change using the class

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const button=document.querySelector('button')
+const bonusButton=document.querySelector('button.bonusCredit')
 const heading=document.querySelector('h1')
+const bonusHeading=document.querySelector('h1.bonusCredit')
 
 function changeHeading(){
     heading.textContent='Clicked'
 }
 
+function changeHeading2(){
+    bonusHeading.textContent='Chris'
+}
+
 button.addEventListener('click', changeHeading)
+bonusButton.addEventListener('click', changeHeading2)
