eee098b6445f5dcc193c1ff89b228760f631f32f
Colin Hayes <colin.hayes985@gmail.com>
2018-06-28 10:13:47 -0400

    Removed JS handling of Day 1 Heading Buttons.

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-const button=document.querySelector('button')
-const bonusButton=document.querySelector('button.bonusCredit');
-const heading=document.querySelector('h1');
-const bonusHeading=document.querySelector('h1.bonusCredit');
 const listItem=document.querySelector('#filmForm');
 const field=document.getElementsByName('filmName');
 var filmArr=[];
 var chrisArr=[];
 var itemArr=[];
 
-function changeHeading(){
-    heading.textContent='Clicked'
-}
 
-function changeHeading2(){
-    bonusHeading.textContent='Chris'
-}
 
 function listItems(ev){
     ev.preventDefault();
@@ -103,8 +93,5 @@ function checkFieldContent(ev,item){
     }
 }
 
-
-button.addEventListener('click', changeHeading);
-bonusButton.addEventListener('click', changeHeading2);
 listItem.addEventListener('submit', listItems); 
 
